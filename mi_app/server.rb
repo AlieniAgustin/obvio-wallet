c6d7f0c7d6c9848a99dc9d0896c6a67607914a9a
ddalio <magd45801@gmail.com>
2025-05-13 21:43:42 -0300

    add logging

--- mi_app/server.rb
+++ mi_app/server.rb
@@ -1,16 +1,23 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require 'logger'
 
 class App < Sinatra::Application
+
   configure :development do
+    enable :logging
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+
     register Sinatra::Reloader
     after_reload do
-      puts 'Reloaded...'
+      logger.info 'Reloaded!!!!'
     end
   end
 
   get '/' do
-    'Welcome to Obvio!'
+    'Welcome to Obvio!!'
   end
 end
