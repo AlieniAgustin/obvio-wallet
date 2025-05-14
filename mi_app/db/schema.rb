d857813fcbff2aef32dbf5d9a306dd0e919d8887
ddalio <magd45801@gmail.com>
2025-05-13 22:37:48 -0300

    corregido models. Tabla users creada en mi migrations

--- mi_app/db/schema.rb
+++ mi_app/db/schema.rb
@@ -11,4 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_14_010603) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
