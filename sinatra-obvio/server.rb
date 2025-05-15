1ddca1f08cff5cdce355b747a23927ed9792a6ed
alieniAgus <150525543+AlieniAgustin@users.noreply.github.com>
2025-05-14 21:09:18 -0300

    add Docker

---
+++ sinatra-obvio/server.rb
@@ -0,0 +1,23 @@
+require 'bundler/setup'
+require 'sinatra/base'
+require 'sinatra/activerecord'
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require_relative 'models/user'
+
+class App < Sinatra::Application
+  configure :development do
+    enable :logging
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+
+    register Sinatra::Reloader
+    after_reload do
+      logger.info 'Reloaded!!!'
+    end
+  end
+
+  get '/' do
+    'Welcome'
+  end
+end
