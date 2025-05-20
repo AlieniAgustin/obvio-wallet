17b2e724e5fa675c961b6736744269fbf6e6161f
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-05-20 11:37:32 -0300

    Agrego modelo y migracion para vouchers

---
+++ sinatra-obvio/db/migrate/20250520140724_create_vouchers.rb
@@ -0,0 +1,17 @@
+class CreateVouchers < ActiveRecord::Migration[7.2]
+  def change
+
+    create_table :vouchers do |t|
+      t.date :dia
+      t.time :hora
+      t.decimal :importe, precision: 10, scale: 2
+      t.string :description
+
+      #Descomentar cuaando se cree la entidad transfer
+      #t.references :transfer, null: false, foreing_key: true, unique: true
+      t.timestamps
+    end
+
+
+  end
+end
