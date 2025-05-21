c7158ad40fa834707ab7a13f4d905e1e0c550662
HernanJara <hernanjara234@gmail.com>
2025-05-21 02:37:55 -0300

    Nueva Migracion monthly_summaries

--- sinatra-obvio/db/migrate/20250521044446_create_monthly_summaries.rb
+++ sinatra-obvio/db/migrate/20250521044446_create_monthly_summaries.rb
@@ -1,7 +1,7 @@
 class CreateMonthlySummaries < ActiveRecord::Migration[7.2]
   def change
     create_table :monthly_summaries do |t|
-      t.references :account, foreign_key: true
+      
       t.decimal :initial_balance, precision: 15, scale: 2, null: false
       t.decimal :final_balance, precision: 15, scale: 2, null: false
       t.text :note
