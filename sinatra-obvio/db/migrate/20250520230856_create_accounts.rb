0a639b8824704063cc6cefbebf99251afd1b703c
AlieniAgustin <alieniagustinweb@gmail.com>
2025-05-20 21:06:55 -0300

    modify CreateAccounts

--- sinatra-obvio/db/migrate/20250520230856_create_accounts.rb
+++ sinatra-obvio/db/migrate/20250520230856_create_accounts.rb
@@ -1,11 +1,13 @@
 class CreateAccounts < ActiveRecord::Migration[7.2]
   def change
     create_table :accounts do |t|
-      t.integer :idAccount    # Columna entera para un id personalizado (opcional)
+      t.integer :id_account    # Columna entera para un id personalizado (opcional)
       t.float :balance        # Columna para saldo
       t.string :email         # Columna para email
       t.string :username      # Columna para nombre de usuario
-      t.string :password      # Columna para contraseña 
+      t.string :password      # Columna para contraseña
+      t.integer :cvu          # Columna para cvu
+      t.string :alias 
       t.timestamps           # Agrega created_at y updated_at automáticamente
     end
   end
