3cd0fbb80c9ad14f55a4241ed1580c33a4312956
AlieniAgustin <alieniagustinweb@gmail.com>
2025-05-20 21:23:03 -0300

    modify createAccounts and createUsers

--- sinatra-obvio/db/migrate/20250514234930_create_users.rb
+++ sinatra-obvio/db/migrate/20250514234930_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-
+      t.string :first_name      #Columna para el nombre
+      t.string :last_name       #Columna para el apellido
+      t.string :dni             #Columna para el dni
+      t.string :address         #Columna para la direccion
       t.timestamps
     end
   end
--- sinatra-obvio/db/migrate/20250520230856_create_accounts.rb
+++ sinatra-obvio/db/migrate/20250520230856_create_accounts.rb
@@ -6,7 +6,7 @@ class CreateAccounts < ActiveRecord::Migration[7.2]
       t.string :email         # Columna para email
       t.string :username      # Columna para nombre de usuario
       t.string :password      # Columna para contraseña
-      t.integer :cvu          # Columna para cvu
+      t.string :cvu          # Columna para cvu
       t.string :alias         # Columna para alias
       t.timestamps           # Agrega created_at y updated_at automáticamente
     end
