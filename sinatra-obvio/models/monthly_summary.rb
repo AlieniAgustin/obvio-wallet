f26e5439239f20a4b76b81b725f2802ed58ef293
HernanJara <hernanjara234@gmail.com>
2025-05-21 02:10:45 -0300

    Agrego el modelo MonthlySummary

---
+++ sinatra-obvio/models/monthly_summary.rb
@@ -0,0 +1,13 @@
+class MonthlySummary < ActiveRecord::Base
+	belongs_to :account
+
+	validates :month, presence: true
+	validates :year, presence: true
+	validates :account_id, presence: true
+	validates :initial_balance, presence: true
+	validates :final_balance, presence: true
+	validates :transaction_acount, presence: true
+	validates :balance_status, presence: true
+
+
+end
