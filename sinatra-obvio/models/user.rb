01c4fbf252332d41b409a4f59cd09dead9957465
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-05-25 01:17:51 -0300

    add bcrypt and email validation to user model

--- sinatra-obvio/models/user.rb
+++ sinatra-obvio/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+        has_secure_password
 	has_one :account #tiene una cuenta
-
+        
+        validates :email, presence: true, uniqueness: true
 	validates :dni, :first_name, :last_name, :address, presence: true #para validar que todos sus campos sean obligatorios
 end
