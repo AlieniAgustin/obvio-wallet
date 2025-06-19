e29607b282189b3c1dcc4c60671819edf904550a
Julian Varea <julianvarea14@gmail.com>
2025-06-19 20:38:24 -0300

    Fix spec_helper

--- sinatra-obvio/spec/spec_helper.rb
+++ sinatra-obvio/spec/spec_helper.rb
@@ -9,6 +9,13 @@ db_config = YAML.load_file(File.expand_path('../config/database.yml', __dir__), 
 ActiveRecord::Base.establish_connection(db_config[ENV['RACK_ENV']])
 
 RSpec.configure do |config|
+  config.around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
