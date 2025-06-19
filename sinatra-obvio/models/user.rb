61604143e9f347b6296b617dcca88592a4357c30
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-06-19 00:49:21 -0300

    validacion de dni unico

--- sinatra-obvio/models/user.rb
+++ sinatra-obvio/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 	has_one :account #tiene una cuenta
         
     validates :email, presence: true, uniqueness: { message: "El mail ya está registrado" }
-	validates :dni, :first_name, :last_name, :address, presence: true #para validar que todos sus campos sean obligatorios
+    validates :dni, presence: true, uniqueness: { message: "El dni ya está registrado" }
+	validates :first_name, :last_name, :address, presence: true #para validar que todos sus campos sean obligatorios
     validates :dni, length: {is:8, message: "el dni debe tener exactamente 8 caracteres"}
     #validacion solo de backend. La del cliente la hace el navegador 
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "formato mail no válido" }
